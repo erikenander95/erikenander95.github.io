1dedaee10980eaa0718587920c7058ca74a23463
erikenander95 <erik.enander95@gmail.com>
2024-05-01 15:09:21 +0200

    landing page

--- js/script.js
+++ js/script.js
@@ -6,11 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
 
       const targetId = link.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
 
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-      });
+      // Check if the target id is not empty (not the Home button)
+      if (targetId !== "") {
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      } else {
+        // Scroll to the top of the page for the Home button
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
     });
   });
 });
