db5a4efbf9470a518feea97d4206068c688070bd
erikenander95 <erik.enander95@gmail.com>
2026-01-05 21:35:43 +0100

    vite.config.ts change

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  base: "/erikenander95.github.io/",
+});
